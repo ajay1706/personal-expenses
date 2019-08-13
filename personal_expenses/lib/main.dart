69441c0511796071731c946fb7e61f0c96dbc45d
ajay1706 <ajay97456@gmail.com>
2019-08-13 19:29:22 +0530

    adding styling to title amount & date

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/transaction.dart';
-main()=>runApp(MyApp());
+
+main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,56 +13,77 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-class MyHomePage extends StatelessWidget{
-  final List<Transaction> transaction =[
-    Transaction(id: 't1',title: 'New Shoes',amount: 69.99,date: DateTime.now()),
-    Transaction(id: 't2',title: 'Weekly Groceries',amount: 16.53,date: DateTime.now())
-
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Personal expenses"),
-      ),
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: <Widget>[
-      Container(
-        width: double.infinity,
-
-        child: Card(
-
-          color: Colors.blueAccent,
-
-           child: Text('CHART!'),
-          elevation: 9,
-
+        appBar: AppBar(
+          title: Text("Personal expenses"),
         ),
-      ),
-   Column(children: transaction.map((tx){
-     return Card(
-       child: Row(
-         children: <Widget>[
-           Container(
-             child: Text(tx.amount.toString()),
-           ),
-           Column(
-             children: <Widget>[
-               Text(tx.title),
-               Text(
-                 tx.date.toString()
-               )
-             ],
-           )
-         ],
-       ),
-     );
-   }).toList(),)
-    ],
-    ));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+
+              width: double.infinity,
+              child: Card(
+
+                color: Colors.blueAccent,
+                child: Text('CHART!'),
+                elevation: 9,
+              ),
+            ),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+
+                        margin:EdgeInsets.symmetric(horizontal:15,
+                  vertical: 10),
+                        decoration:BoxDecoration(
+                          border: Border.all(color: Colors.redAccent,width: 2)
+                        ) ,
+
+                        padding: const EdgeInsets.all(10),
+                        child: Text(tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.redAccent
+
+                        ),),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(tx.title,style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          Text(tx.date.toString(),style: TextStyle(
+                            color: Colors.grey
+                          ),)
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
