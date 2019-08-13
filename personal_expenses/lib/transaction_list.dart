64bde24a6b0d80372426c15e7958bb6ed01ecc15
ajay1706 <ajay97456@gmail.com>
2019-08-13 23:51:55 +0530

    changing scrollView to ListView

--- personal_expenses/lib/transaction_list.dart
+++ personal_expenses/lib/transaction_list.dart
@@ -13,50 +13,52 @@ TransactionList(this.transactions);
     return
       Container(
         height: 500,
-        child: SingleChildScrollView(
-          child: Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      
 
-                      margin:EdgeInsets.symmetric(horizontal:15,
-                          vertical: 10),
-                      decoration:BoxDecoration(
-                          border: Border.all(color: Colors.redAccent,width: 2)
-                      ) ,
+          child: ListView(
 
-                      padding: const EdgeInsets.all(10),
-                      child: Text("\$ ${tx.amount}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                            color: Colors.redAccent
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        
 
-                        ),),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(tx.title,style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                        ),),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
+                        margin:EdgeInsets.symmetric(horizontal:15,
+                            vertical: 10),
+                        decoration:BoxDecoration(
+                            border: Border.all(color: Colors.redAccent,width: 2)
+                        ) ,
+
+                        padding: const EdgeInsets.all(10),
+                        child: Text("\$ ${tx.amount}",
                           style: TextStyle(
-                              color: Colors.grey
-                          ),)
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              color: Colors.redAccent
+
+                          ),),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(tx.title,style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                          ),),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(
+                                color: Colors.grey
+                            ),)
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          
+        
       );
   }
 }
