769ee35a21d7eac4cf3b8a82f626c70939b2ce70
ajay1706 <ajay97456@gmail.com>
2019-08-13 20:30:02 +0530

    adding user and new transactions file

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/models/transaction.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses/transaction_list.dart';
 
 main() => runApp(MyApp());
 
@@ -86,49 +87,8 @@ controller: amountController,
                 ),
               ),
             ),
+TransactionList(),
 
-
-            Column(
-              children: transaction.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-
-                        margin:EdgeInsets.symmetric(horizontal:15,
-                  vertical: 10),
-                        decoration:BoxDecoration(
-                          border: Border.all(color: Colors.redAccent,width: 2)
-                        ) ,
-
-                        padding: const EdgeInsets.all(10),
-                        child: Text("\$ ${tx.amount}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.redAccent
-
-                        ),),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(tx.title,style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),),
-                          Text(
-                           DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                            color: Colors.grey
-                          ),)
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
           ],
         ));
   }
--- personal_expenses/lib/transaction_list.dart
+++ personal_expenses/lib/transaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatefulWidget {
   @override
@@ -20,6 +21,47 @@ class _TransactionListState extends State<TransactionList> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return
+      Column(
+        children: _userTransaction.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+
+                  margin:EdgeInsets.symmetric(horizontal:15,
+                      vertical: 10),
+                  decoration:BoxDecoration(
+                      border: Border.all(color: Colors.redAccent,width: 2)
+                  ) ,
+
+                  padding: const EdgeInsets.all(10),
+                  child: Text("\$ ${tx.amount}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.redAccent
+
+                    ),),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(tx.title,style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                    ),),
+                    Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(
+                          color: Colors.grey
+                      ),)
+                  ],
+                )
+              ],
+            ),
+          );
+        }).toList(),
+      );
   }
 }
