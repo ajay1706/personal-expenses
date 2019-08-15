cd7f7d6981ef7617624b5403d111533cb20e7706
ajay1706 <ajay97456@gmail.com>
2019-08-16 00:03:44 +0530

    deleting cupertinoApp and continuing with material app

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -17,7 +17,7 @@ main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Platform.isIOS ?CupertinoApp() :MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.green,
