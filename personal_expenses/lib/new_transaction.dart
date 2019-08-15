71002bdbbab539c159a88e14368f69c528119bce
ajay1706 <ajay97456@gmail.com>
2019-08-15 23:24:18 +0530

    add view changes to the keyboard input

--- personal_expenses/lib/new_transaction.dart
+++ personal_expenses/lib/new_transaction.dart
@@ -53,62 +53,69 @@ Navigator.of(context).pop();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
 
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                  labelText: 'Title'
-              ),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom+  10
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Title'
+                ),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData,
 
 //                      onChanged: (val) => amountInput=val,
 
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  labelText: 'Amount'
               ),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData,
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Amount'
+                ),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData,
 //                      onChanged: (val){
 //                        titleInput = val;
 //                      },
-            ),
-
-            Container(
-              height: 70,
-              child: Row(
-
-                children: <Widget>[
-                  Expanded(child: Text(_selectedDate == null ? "No Date Chosen" : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}")),
-                  FlatButton(
-
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text("Choose Date",style:
-                      TextStyle(fontWeight:FontWeight.bold ),),
-                    onPressed:_presentDatePicker,
-                  )
-                ],
               ),
-            ),
 
-            RaisedButton(
-                child: Text("Add Transaction"),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).textTheme.button.color,
-                onPressed: _submitData
+              Container(
+                height: 70,
+                child: Row(
 
-            )
+                  children: <Widget>[
+                    Expanded(child: Text(_selectedDate == null ? "No Date Chosen" : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}")),
+                    FlatButton(
 
-          ],
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text("Choose Date",style:
+                        TextStyle(fontWeight:FontWeight.bold ),),
+                      onPressed:_presentDatePicker,
+                    )
+                  ],
+                ),
+              ),
+
+              RaisedButton(
+                  child: Text("Add Transaction"),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button.color,
+                  onPressed: _submitData
+
+              )
+
+            ],
+          ),
         ),
       ),
     );
